66ebecb8acd6efffd4f16e6a3f8d5d1b81d78dbf
Mayara Felix <maylc.dev@gmail.com>
2021-09-08 08:43:26 -0400

    Rename properties to follow Kotlin standards.

--- ContactsList/app/src/main/java/io/github/maylcf/contactslist/helpers/HelperDB.kt
+++ ContactsList/app/src/main/java/io/github/maylcf/contactslist/helpers/HelperDB.kt
@@ -13,26 +13,26 @@ class HelperDB(context: Context) : SQLiteOpenHelper(context, dbName, null, versi
         private const val version = 2
     }
 
-    val TABLE_NAME = "contato"
-    val COLUMNS_ID = "id"
-    val COLUMNS_NOME = "nome"
-    val COLUMNS_TELEFONE = "telefone"
-    val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
-    val CREATE_TABLE = "CREATE TABLE $TABLE_NAME (" +
-            "$COLUMNS_ID INTEGER NOT NULL," +
-            "$COLUMNS_NOME TEXT NOT NULL," +
-            "$COLUMNS_TELEFONE TEXT NOT NULL," +
-            "" +
-            "PRIMARY KEY($COLUMNS_ID AUTOINCREMENT)" +
+    private val tableName = "contato"
+    private val columnId = "id"
+    private val columnName = "nome"
+    private val columnPhone = "telefone"
+
+    private val dropTableSql = "DROP TABLE IF EXISTS $tableName"
+    private val createTableSql = "CREATE TABLE $tableName (" +
+            "$columnId INTEGER NOT NULL," +
+            "$columnName TEXT NOT NULL," +
+            "$columnPhone TEXT NOT NULL," +
+            "PRIMARY KEY($columnId AUTOINCREMENT)" +
             ")"
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(CREATE_TABLE)
+        db?.execSQL(createTableSql)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         if (oldVersion != newVersion) {
-            db?.execSQL(DROP_TABLE)
+            db?.execSQL(dropTableSql)
         }
         onCreate(db)
     }
@@ -40,28 +40,33 @@ class HelperDB(context: Context) : SQLiteOpenHelper(context, dbName, null, versi
     fun searchContact(search: String, isSearchingById: Boolean = false): List<Contact> {
         val db = readableDatabase ?: return mutableListOf()
         val list = mutableListOf<Contact>()
-        var where: String? = null
-        var args: Array<String> = arrayOf()
+        val where: String?
+        val args: Array<String>
+
         if (isSearchingById) {
-            where = "$COLUMNS_ID = ?"
-            args = arrayOf("$search")
+            where = "$columnId = ?"
+            args = arrayOf(search)
         } else {
-            where = "$COLUMNS_NOME LIKE ?"
+            where = "$columnName LIKE ?"
             args = arrayOf("%$search%")
         }
-        val cursor = db.query(TABLE_NAME, null, where, args, null, null, null)
+
+        val cursor = db.query(tableName, null, where, args, null, null, null)
+
         if (cursor == null) {
             db.close()
             return mutableListOf()
         }
+
         while (cursor.moveToNext()) {
-            val contato = Contact(
-                cursor.getInt(cursor.getColumnIndex(COLUMNS_ID)),
-                cursor.getString(cursor.getColumnIndex(COLUMNS_NOME)),
-                cursor.getString(cursor.getColumnIndex(COLUMNS_TELEFONE))
+            val contact = Contact(
+                cursor.getInt(cursor.getColumnIndex(columnId)),
+                cursor.getString(cursor.getColumnIndex(columnName)),
+                cursor.getString(cursor.getColumnIndex(columnPhone))
             )
-            list.add(contato)
+            list.add(contact)
         }
+
         db.close()
         return list
     }
@@ -69,15 +74,15 @@ class HelperDB(context: Context) : SQLiteOpenHelper(context, dbName, null, versi
     fun saveContact(contact: Contact) {
         val db = writableDatabase ?: return
         val content = ContentValues()
-        content.put(COLUMNS_NOME, contact.name)
-        content.put(COLUMNS_TELEFONE, contact.phone)
-        db.insert(TABLE_NAME, null, content)
+        content.put(columnName, contact.name)
+        content.put(columnPhone, contact.phone)
+        db.insert(tableName, null, content)
         db.close()
     }
 
     fun deleteContact(id: Int) {
         val db = writableDatabase ?: return
-        val sql = "DELETE FROM $TABLE_NAME WHERE $COLUMNS_ID = ?"
+        val sql = "DELETE FROM $tableName WHERE $columnId = ?"
         val arg = arrayOf("$id")
         db.execSQL(sql, arg)
         db.close()
@@ -85,7 +90,7 @@ class HelperDB(context: Context) : SQLiteOpenHelper(context, dbName, null, versi
 
     fun updateContact(contact: Contact) {
         val db = writableDatabase ?: return
-        val sql = "UPDATE $TABLE_NAME SET $COLUMNS_NOME = ?, $COLUMNS_TELEFONE = ? WHERE $COLUMNS_ID = ?"
+        val sql = "UPDATE $tableName SET $columnName = ?, $columnPhone = ? WHERE $columnId = ?"
         val arg = arrayOf(contact.name, contact.phone, contact.id)
         db.execSQL(sql, arg)
         db.close()
